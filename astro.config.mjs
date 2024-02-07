2260681698d6ce48045dd927be4d36f5bb8a78e4
Joerg Schoeneburg <schoeneburg.j@googlemail.com>
2024-02-07 14:33:50 +0100

    add astri icons and remove the other icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,17 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind({
-			applyBaseStyles: false
-		}),
-		sitemap()
-	],
-	compressHTML: false,
-	output: 'static',
-	build: {
-		assets: '__assets'
-	}
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), sitemap(), icon()],
+  compressHTML: false,
+  output: 'static',
+  build: {
+    assets: '__assets'
+  }
 });
