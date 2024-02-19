63229ee7010f861df847452c301834d6ba951e15
Joerg Schoeneburg <schoeneburg.j@googlemail.com>
2024-02-19 12:37:27 +0100

    added option for nesting

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,21 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
-import icon from "astro-icon";
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    applyBaseStyles: false
-  }), sitemap(), icon()],
-  compressHTML: false,
-  output: 'static',
-  build: {
-    assets: '__assets'
-  }
+	integrations: [
+		tailwind({
+			nesting: true,
+			applyBaseStyles: false
+		}),
+		sitemap(),
+		icon()
+	],
+	compressHTML: false,
+	output: 'static',
+	build: {
+		assets: '__assets'
+	}
 });
