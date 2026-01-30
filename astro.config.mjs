cf620bb6f05e70d9b3dfb53c231bde76b35165fb
jswebschmiede <schoeneburg.j@googlemail.com>
2026-01-30 23:51:57 +0100

    Enhanced Astro configuration by adding path aliases for improved module resolution. Updated pnpm-lock.yaml to reflect dependency version changes. Refactored styles in various components for better consistency and responsiveness, including adjustments to animations and utility classes. Removed unused theme and utility CSS files to streamline the project.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
+import path from 'path';
+
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,7 +14,16 @@ export default defineConfig({
         mdx()
     ],
     vite: {
-        plugins: [tailwindcss()]
+        plugins: [tailwindcss()],
+        resolve: {
+            alias: {
+                '@styles': path.resolve('./src/styles/components'),
+                '@components': path.resolve('./src/components'),
+                '@layouts': path.resolve('./src/layouts'),
+                '@assets': path.resolve('./src/assets'),
+                '@scripts': path.resolve('./src/scripts/components')
+            }
+        }
     },
     compressHTML: true,
     output: 'static',
